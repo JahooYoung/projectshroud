8b4c71b735ebd42865224b877151bc57156eefd3
MarkYu98 <markyu@pku.edu.cn>
2019-03-30 17:14:37 +0800

    fix typo

--- backend/views.py
+++ backend/views.py
@@ -110,7 +110,7 @@ class UserEventRegister(generics.CreateAPIView):
         data = request.data
         if 'user_id' in data:
             user = get_user_model().objects.get(id=data['user_id'])
-        if 'event_id' in data:
+        if 'event_id' not in data:
             return Response(status=status.HTTP_400_BAD_REQUEST, data={'msg': 'No event_id specified.'})
         event = Event.objects.get(id=data.get('event_id'))
         if 'transport_id' not in data or data.get('transport_id') is None:
