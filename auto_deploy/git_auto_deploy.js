1cb76ba1321a63d3cbe6485ed0728ebe46ab0736
MarkYu98 <markyu98@outlook.com>
2019-04-24 13:01:31 +0800

    auto deploy test

--- auto_deploy/git_auto_deploy.js
+++ auto_deploy/git_auto_deploy.js
@@ -1,58 +1,61 @@
-const http = require('http')
-const createHandler = require('github-webhook-handler')
-const spawn = require('child_process').spawn
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const spawn = require('child_process').spawn;
 
 function run_cmd(cmd, args, callback) {
-  const child = spawn(cmd, args)
-  let resp = ''
-  let finished = 0
+  const child = spawn(cmd, args);
+  let resp = '';
+  let finished = 0;
 
-  child.stderr.on('data', buffer => resp += buffer.toString())
-  child.stdout.on('data', buffer => resp += buffer.toString())
+  child.stderr.on('data', buffer => resp += buffer.toString());
+  child.stdout.on('data', buffer => resp += buffer.toString());
   child.stderr.on('end', () => {
     if (++finished === 2) {
-      callback(resp)
+      callback(resp);
     }
-  })
+  });
   child.stdout.on('end', () => {
     if (++finished === 2) {
-      callback(resp)
+      callback(resp);
     }
-  })
+  });
 }
 
 const handler = createHandler({
     path: '/github-push',
     secret: 'shroud'
-})
+});
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
+http.createServer((req, res) => {
+  handler(req, res, err => {
+    res.statusCode = 404;
+    res.end('no such location');
   })
-}).listen(8765)
+}).listen(8765);
 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
+handler.on('error', err => {
+  console.error('Error:', err.message);
+});
 
-handler.on('push', function (event) {
+handler.on('push', event => {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref)
+    event.payload.ref);
   if (event.payload.ref !== 'refs/heads/site') {
-    return
+    return;
   }
-  let frontendUpdated = false
-  ['added', 'removed', 'modified'].forEach(ele => {
-    event.payload.commits[ele].forEach(file => {
-      if (/^frontend/.test(file))
-        frontendUpdated = true
-    })
-  })
-  const args = []
+  let frontendUpdated = false;
+  event.payload.commits.forEach(commit => {
+    ['added', 'removed', 'modified'].forEach(ele => {
+      commit[ele].forEach(file => {
+        if (/^frontend/.test(file))
+          frontendUpdated = true;
+      });
+    });
+  });
+
+  const args = [];
   if (frontendUpdated)
-    args.push('yarn')
-  run_cmd('sh', ['./git_auto_deploy.sh', ...args], text => console.log(text))
-})
+    args.push('yarn');
+  run_cmd('sh', ['./git_auto_deploy.sh', ...args], text => console.log(text));
+});
