b65f94692f7427c791b46cdb9ee02e51805f6a0a
MarkYu98 <markyu@pku.edu.cn>
2019-04-24 14:32:04 +0800

    update deploy shell

--- auto_deploy/git_auto_deploy.sh
+++ auto_deploy/git_auto_deploy.sh
@@ -2,23 +2,28 @@ cd /home/projectshroud
 git checkout site
 git reset --hard HEAD
 git pull
-while getopts ":f:b" opt
+while getopts ":ypfb" opt
 do
     case $opt in
         f)
-        if [ {$OPTARG} == 'build' ]; then
-            echo "Building frontend"
-            (cd frontend; yarn build)
-        elif [ {$OPTARG}x = 'install'x ]; then
-            echo "Installing frontend dependencies and building"
-            (cd frontend; yarn install; yarn build)
-        fi
+        echo "Building frontend"
+        (cd frontend; yarn build)
         ;;
         b)
         echo "Migrating backend"
         source /home/pyweb/bin/activate
         python manage.py makemigrations
         python manage.py migrate
+        deactivate
+        ;;
+        y)
+        echo "Installing frontend dependencies and building"
+        (cd frontend; yarn install; yarn build)
+        ;;
+        p)
+        echo "Installing python dependencies"
+        source /home/pyweb/bin/activate
+        pip install -r requirements.pip
         deactivate
         ;;
     esac
