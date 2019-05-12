059f5039c64b9d006c334e82423edd3a630647ab
Jahoo <jahoo@pku.edu.cn>
2019-05-13 00:30:32 +0800

    add assign admin support

--- frontend/src/views/EventAdmin/Attendee.vue
+++ frontend/src/views/EventAdmin/Attendee.vue
@@ -76,6 +76,32 @@
               </b-button>
             </b-popover>
           </template>
+
+          <template
+            slot="is_admin"
+            slot-scope="row"
+          >
+            <font-awesome-icon
+              v-if="row.value"
+              :id="'checkin-popover-' + row.item.user_info.id"
+              icon="check"
+              :style="{ color: 'green' }"
+            />
+            <font-awesome-icon
+              v-else
+              :id="'checkin-popover-' + row.item.user_info.id"
+              icon="times"
+              :style="{ color: 'red' }"
+            />
+            <font-awesome-icon
+              v-if="!row.value"
+              :id="'checkin-popover-' + row.item.user_info.id"
+              icon="plus"
+              :style="{ color: 'blue' }"
+              class="ml-3"
+              @click="assignAdmin(row.item)"
+            />
+          </template>
         </b-table>
       </template>
     </TableLayout>
@@ -85,10 +111,10 @@
 <script>
 import TableLayout from '@/components/TableLayout.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faCheck, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faTimes, faCheck)
+library.add(faTimes, faCheck, faPlus)
 
 export default {
   name: 'EventAdminAttendee',
@@ -119,9 +145,18 @@ export default {
         {
           key: 'checked_in',
           label: 'Checked in'
+        },
+        {
+          key: 'is_admin',
+          label: 'Is Admin'
         }
       ],
       attendee: []
+    }
+  },
+  computed: {
+    eventId () {
+      return this.$route.params.id
     }
   },
   created () {
@@ -129,13 +164,22 @@ export default {
   },
   methods: {
     refresh () {
-      this.axios.get('/api/event/' + this.$route.params.id + '/attendee/')
+      this.axios.get(`/api/event/${this.eventId}/attendee/`)
         .then(res => {
           this.attendee = res.data
         })
     },
     manualCheckIn (rowItem) {
       // Todo
+    },
+    assignAdmin (rowItem) {
+      this.axios.post('/api/assignadmin/', {
+        user_id: rowItem.user_info.id,
+        event_id: this.eventId
+      })
+        .then(() => {
+          rowItem.is_admin = true
+        })
     }
   }
 }
